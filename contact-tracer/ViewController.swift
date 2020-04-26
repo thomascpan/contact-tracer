f5dd05915f54790871f4a3d139dd1df4b8f928c8
Thomas Pan <thomascpan@gmail.com>
2020-04-25 23:45:05 -0700

    Initial Commit

---
+++ contact-tracer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contact-tracer
+//
+//  Created by Thomas Pan on 4/25/20.
+//  Copyright © 2020 Thomas Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
